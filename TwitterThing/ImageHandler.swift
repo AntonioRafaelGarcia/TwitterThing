1694f9a3f6006fc6f77f345a881147f2b9c2f00e
Antonio <argarcia28@gmail.com>
2015-11-06 13:38:37 -0800

    added tweet cell label connections in xib and other constraints

---
+++ TwitterThing/ImageHandler.swift
@@ -0,0 +1,26 @@
+////
+////  ImageHandler.swift
+////  TwitterThing
+////
+////  Created by Antonio Garcia on 11/2/15.
+////  Copyright © 2015 Patriarch Inc. All rights reserved.
+////
+//
+//import Foundation
+//import UIKit
+//
+//class ImageHandler {
+//    
+//    // instantiating Singleton
+//    static let sharedService = ImageHandler()
+//    
+//    class func fetchImage(profileImageUrl: String, completion: (String?, UIImage?) -> ()){
+//        if let url = NSURL(string: profileImageUrl) {
+//            let queue = dispatch_queue_create("desired name", nil)
+//            dispatch_async(queue)
+//                {
+//                    if let something =  NSData(contentsOfURL: url) {
+//            }
+//        }
+//    }
+//}
--- TwitterThing/TweetCell.swift
+++ TwitterThing/TweetCell.swift
@@ -19,6 +19,9 @@ class TweetCell: UITableViewCell {
         didSet {
             
             if let tweet = self.tweet, user = tweet.user, text = self.tweet?.text {
+            
+                print(self.tweet?.user?.profileImageUrl)
+                
                 
                 self.tweetTextLabel.text = text
                 
@@ -27,7 +30,7 @@ class TweetCell: UITableViewCell {
                 } else {
                     if let url = NSURL(string: user.profileImageUrl) {
                         let downloadQ = dispatch_queue_create("downloadQ", nil)
-                        dispatch_async(downloadQ, { () Void in
+                        dispatch_async(downloadQ, { () -> Void in
                             let imageData = NSData(contentsOfURL: url)!
                         
                         dispatch_async(dispatch_get_main_queue(), { () -> Void in
